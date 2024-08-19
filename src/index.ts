1e607e2cfd123dcad048b56effd3363c1cfe2d9b
Daniel Nashed <dnlnashed@gmail.com>
2024-08-19 18:21:21 -0400

    install packages, set up initial file testing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-// without this import, this file doesn't end up in the build
-import './css/style.css';
+// // without this import, this file doesn't end up in the build
+// import './css/style.css';
 
-import { GoodTimes } from '@/internal';
+// import { GoodTimes } from '@/internal';
 
-// everything after is what executes at startup
+// // everything after is what executes at startup
 
-const givver = new GoodTimes();
-givver.doWork();
+// const givver = new GoodTimes();
+// givver.doWork();
+
+import shp from 'shpjs/dist/shp.min.js';
+import fgbgeojson from 'flatgeobuf/dist/flatgeobuf-geojson.min.js';
+
+async function processFiles() {
+    // Shapefile
+    console.time('Shapefile');
+    const shpResponse = await fetch('/file-layers/AirEmissions_PM25_2020.zip');
+    const shpData = await shpResponse.arrayBuffer();
+    const parsedShp = await shp(shpData);
+    console.log(parsedShp);
+    console.timeEnd('Shapefile');
+
+    // Flatgeobuf
+    console.time('FlatGeobuf');
+    const fgbResponse = await fetch('/file-layers/AirEmissions_PM25_2020.fgb');
+    const fgbBuffer = await fgbResponse.arrayBuffer();
+    const view = new Uint8Array(fgbBuffer);
+    const features = fgbgeojson.deserialize(view);
+    console.log(features);
+    console.timeEnd('FlatGeobuf');
+}
+
+processFiles();
---
+++ src/shims-vue.d.ts
@@ -0,0 +1,3 @@
+declare module 'shpjs/dist/shp.min.js';
+declare module 'flatgeobuf/dist/flatgeobuf.min.js';
+declare module 'flatgeobuf/dist/flatgeobuf-geojson.min.js';
