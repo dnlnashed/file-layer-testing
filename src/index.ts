3808f6fb6888cc9549e596baa264b7720ff5c023
Daniel Nashed <dnlnashed@gmail.com>
2024-08-22 15:56:06 -0400

    added zipped fgb file and unzipping in code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import shp from 'shpjs/dist/shp.min.js';
 import fgbgeojson from 'flatgeobuf/dist/flatgeobuf-geojson.min.js';
+import JSZip from 'jszip';
 
 async function processFiles() {
     // Shapefile
@@ -10,20 +11,23 @@ async function processFiles() {
     const shpData = await shpResponse.arrayBuffer();
     const parsedShp = await shp(shpData);
     const shpEnd = performance.now();
-    console.log(parsedShp);
     console.log(shpEnd);
+    console.log(parsedShp);
 
     // Flatgeobuf
     console.log('FlatGeobuf');
+    const zip = new JSZip();
     const fgbStart = performance.now();
     console.log(fgbStart);
-    const fgbResponse = await fetch('/file-layers/AirEmissions_PM25_2020.fgb');
+    const fgbResponse = await fetch('/file-layers/AirEmissions_PM25_2020_fgb.zip');
     const fgbBuffer = await fgbResponse.arrayBuffer();
-    const fgbData = new Uint8Array(fgbBuffer);
+    const unzipped = await zip.loadAsync(fgbBuffer);
+    const fileName = Object.keys(unzipped.files)[0];
+    const fgbData = await unzipped.file(fileName)?.async('uint8array');
     const parsedFgb = await fgbgeojson.deserialize(fgbData);
     const fgbEnd = performance.now();
-    console.log(parsedFgb);
     console.log(fgbEnd);
+    console.log(parsedFgb);
 
     // Calculate the time taken for each operation
     const flatGeobufTime = fgbEnd - fgbStart;
